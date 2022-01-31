8b20867bc23bee3f23e56a9091509f795496a5c9
leasegal <leas@webschool.co.il>
2022-01-31 21:49:14 +0200

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types'
+import windows from '../assets/windows.png'
+
+const Header = ({title, subTitle}) => {
+    return (
+        <div className="headerCV">
+           <div className='titleHeaderCV'>
+               <h1>{title}</h1>
+               <h2>{subTitle}</h2>
+           </div>          
+           <div><img src={windows} /></div>
+       </div>
+    )
+}
+
+
+export default Header
